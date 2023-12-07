57c69b89af88a51eaa5500e31827cb96e1783e4e
user <user@Noutbuk-lkg2n5w43x.local>
2023-12-07 12:23:21 +0200

    docker

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -7,7 +7,64 @@ const router = express.Router();
 
 router.use(checkAuth);
 
+/**
+ * @openapi
+ * '/api/task':
+ *   post:
+ *     tags:
+ *      - Task
+ *     summary: Create a task
+ *     security:
+ *      - basicAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 default: Buy a book
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad Request
+ */
+
 router.post('/task', taskController.createTask);
+
+/**
+ * @openapi
+ * '/api/tasks':
+ *   get:
+ *     tags:
+ *       - Task
+ *     summary: Get tasks
+ *     security:
+ *       - basicAuth: []
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   completed:
+ *                     type: boolean
+ *                   createdBy:
+ *                     type: string
+ *       400:
+ *         description: Bad Request
+ */
 router.get('/tasks', taskController.getTasksByUserId);
 router.get('/tasks/all', checkAdmin, taskController.getAllTasks);
 router.get('/tasks/:id', taskController.getTask);
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUI from 'swagger-ui-express';
 import './config/db.js';
 
+
+// Routes
 import authRouter from './routes/authRoute.js';
 import taskRouter from './routes/taskRoute.js';
+import swaggerSpec from './config/swagger.js';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use('/api', authRouter);
 app.use('/api', taskRouter);
